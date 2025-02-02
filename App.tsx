a84591cf87374813e0554a8e289d8c38c60dd278
shjeng <ghsmsl20@gmail.com>
2025-02-02 20:51:02 +0900

    [2-2] 이벤트 처리하기
    - 리액트에서는 e.target.value를 사용해야 했지만, 네이티브는 props에 text로 바로 들어옴.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Button,
   SafeAreaView,
@@ -9,15 +9,18 @@ import {
 } from 'react-native';
 
 function App(): React.JSX.Element {
+  const [name, setName] = useState('');
+  const handleChangeInput = (text: string) => {
+    console.log(text);
+    // 리액트에서는 e.target.value -> 네이티브는 props에 text로 바로 들어옴.
+    setName(text);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-        <Text>텍스트</Text>
-        <TextInput style={styles.input} />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text>텍스트</Text>
-        <TextInput style={styles.input} />
+        <Text>이름</Text>
+        <TextInput style={styles.input} value={name} onChangeText={handleChangeInput}/>
       </View>
     </SafeAreaView>
   );
@@ -26,21 +29,18 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
   },
   input: {
     flex: 1,
     borderWidth: 2,
     borderColor: 'black',
-    height: 100,
+    height: 50,
     width: 100,
   },
   inputContainer: {
     flex: 1,
-    backgroundColor: 'red',
     flexDirection: 'row', // column이 기본 값
     alignItems: 'center',
-    justifyContent: 'space-evenly',
   }
 });
 
