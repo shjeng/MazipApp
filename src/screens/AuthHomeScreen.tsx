bb8d184833bace31bd80380bfc2510fd4ba924ae
shjeng <ghsmsl20@gmail.com>
2025-02-03 22:37:49 +0900

    네비게이션 타이핑  *   createStackNavigator란? (React Navigation에서 Stack Navigator의 역할)    createStackNavigator는 React Navigation 라이브러리에서 화면을 "스택(Stack)" 방식으로 관리하는 네비게이션 컨테이너를 생성하는 함수입니다.    즉, 사용자가 여러 화면을 이동할 때 이전 화면이 사라지지 않고 스택처럼 쌓이는 구조를 관리합니다.
       * 설명
       createStackNavigator<AuthStackParamList>()
       createStackNavigator를 호출하여 Stack Navigator 객체(Stack)를 생성.
       <AuthStackParamList>는 타입스크립트에서 네비게이션에 전달할 수 있는 매개변수를 타입으로 정의하는 역할 (TSX 코드일 경우).
    
    * screen의 props에 자동으로 StackScreenProps를 넘겨줌.
    StackScreenProps의 타입은 createStackNavigator의 타입과 같음.

--- src/screens/AuthHomeScreen.tsx
+++ src/screens/AuthHomeScreen.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Button, SafeAreaView, StyleSheet, View} from 'react-native';
+import {StackScreenProps} from '@react-navigation/stack';
+import {AuthStackParamList} from '../navigation/AuthStackNavigator.tsx';
+import {authNavigations} from '../constants';
 
-const AuthHomeScreen = ({navigation}) => {
+type AuthHomeScreenProps = StackScreenProps<AuthStackParamList, typeof authNavigations.AUTH_HOME>;
+
+const AuthHomeScreen = ({navigation}: AuthHomeScreenProps) => {
   return (
     <SafeAreaView>
       <View>
-        <Button title="로그인 화면으로 이동" onPress={() => navigation.navigate('Login')}/>
+        <Button title="로그인 화면으로 이동" onPress={() => navigation.navigate(authNavigations.LOGIN)}/>
       </View>
     </SafeAreaView>
   );
